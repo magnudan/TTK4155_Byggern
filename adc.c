804f8073fa4ab39207126ccbb82a56b327559ac2
jostlowe <jostlowe@gmail.com>
2018-09-12 17:36:47 +0200

    added 60us delay in adc read

--- adc.c
+++ adc.c
@@ -5,7 +5,7 @@ volatile char *ext_adc = (char *) 0x1400;
 
 int adc_read_channel(unsigned int channel){
     ext_adc[0] = (uint8_t)(channel+3);
-    _delay_us(1);
+    _delay_us(60);
     volatile uint8_t readout = ext_adc[0];
     _delay_us(1);
     return readout;
