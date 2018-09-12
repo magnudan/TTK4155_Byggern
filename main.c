5940b01de163c6c74b75be7dae19de9f6544e8b7
jostlowe <jostlowe@gmail.com>
2018-09-12 17:30:27 +0200

    wolololol

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 // POLLING EXAMPLE(ATmega162)
 // 8 buttons connected to PORTA, 8 leds to PORTB
 #include "cpu_info.h"
-
+#include "sram.h"
+#include "uart_driver.h"
+#include "adc.h"
+#include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
+
 int main(void){
-    // Buttons input (the whole port)
-    DDRC = 0xFF;
-    while(1){
-        PORTC = 0xFF;
-        _delay_ms(100);
-        PORTC = 0x00;
-        _delay_ms(100);
+    uart_init();
+    SRAM_init();
+    SRAM_test();
+    while (1) {
+        volatile char readout[] = " ";
+        printf("X: ");
+        printf("%d", adc_read_channel(JOYSTICK_X));
+        printf("\tY: ");
+        printf("%d", adc_read_channel(JOYSTICK_Y));
+        printf("\tSlider L: ");
+        printf("%d", adc_read_channel(SLIDER_L));
+        printf("\tSlider R: ");
+        printf("%d", adc_read_channel(SLIDER_R));
+        printf("\n\r");
+        _delay_ms(200);
     }
 }
