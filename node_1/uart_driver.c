a8f3a7e6229b7e7466f627845b543856610342ac
jostlowe <jostlowe@wilma.wilma>
2018-11-07 14:11:25 +0100

    cleaned up uart driver files

--- node_1/uart_driver.c
+++ node_1/uart_driver.c
@@ -1,32 +1,56 @@
+// Header include
 #include "uart_driver.h"
-
-#include <stdio.h>
-#include <avr/io.h>
-#include <stdint.h>
 #include "node_setup.h"
 
 
-#define BAUD 9600
-#define BAUDRATE ( (F_CPU) / (BAUD * 16UL) - 1 )
+// Library includes
+#include <stdio.h>
+#include <avr/io.h>
+#include <stdint.h>
 
+
+// Globals define
 FILE *uart;
 
-void uart_init(){
-    UBRR0H = (BAUDRATE >> 8);       // set high register of the baud rate register UBRR0
-    UBRR0L = BAUDRATE;              // set low register of the baud rate register UBRR0
-    UCSR0B |= (1 << TXEN0) | (1 << RXEN0);      //enable transmit and recieve
-    UCSR0C |= (1 << URSEL0) | (1 << UCSZ00) | (1 << UCSZ01); // Trasmit size 8 bit - 2 stop bits
 
+void uart_init()
+{
+    // Set the high and low baudrate registers to reflect the BAUD defined 
+    UBRR0H = (BAUDRATE >> 8);      
+    UBRR0L = BAUDRATE;              
+
+    // Enable transmit and recieve
+    write_bit(1, UCSR0B, TXEN0);
+    write_bit(1, UCSR0B, RXEN0);
+
+    // Set transmission size to 8 bits 
+    write_bit(1, UCSR0C, UCSZ00);
+    write_bit(1, UCSR0C, UCSZ01);
+
+    // Set stop bits to 2 bits
+    write_bit(1, UCSR0C, URSEL0);
+
+    // Open a stream to use fprint function
     uart = fdevopen(&uart_transmit, &uart_recieve);
 }
 
-int uart_transmit(char data, FILE *f){
-    while (!( UCSR0A & (1 << UDRE0)));      //wait for empty data register UDR0
+
+int uart_transmit(char data, FILE *f)
+{
+    //wait for empty data register UDR0
+    while (!(test_bit(UCSR0A, UDRE0)));
+    
+    //load character data into uart send buffer
     UDR0 = data;
-    return 0;                      //fill UDR0 with data
+    return 0;
 }
 
-unsigned char uart_recieve(void){
-    while(!(UCSR0A) & (1 << RXC0));         //wait for empty data register UDR0
-    return UDR0;                            //fetch UDR0 data
+
+int uart_recieve(FILE *f)
+{
+    //wait for data recieved flag
+    while(!(test_bit(UCSR0A, RXC0)));         
+
+    //fetch UDR0 data
+    return UDR0;                            
 }
--- node_1/uart_driver.h
+++ node_1/uart_driver.h
@@ -1,14 +1,13 @@
 #ifndef uart_driver_H_
 #define uart_driver_H_
-
 #include <stdio.h>
-
 #endif
 
-void uart_init (void);
-
-int uart_transmit (char data, FILE *f);
-
-unsigned char uart_recieve (void);
+#define BAUD 9600
+#define BAUDRATE ( (F_CPU) / (BAUD * 16UL) - 1 )
 
 extern FILE *uart;
+
+void uart_init (void);
+int uart_transmit (char data, FILE *f);
+int uart_recieve (FILE *f);
