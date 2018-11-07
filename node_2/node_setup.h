dc629bb292aaf680088fc020235ae3d43ee5a99f
jostlowe <jostlowe@wilma.wilma>
2018-11-07 09:02:40 +0100

    added write_bit macro to both nodes

--- node_2/node_setup.h
+++ node_2/node_setup.h
@@ -1,9 +1,10 @@
 #ifndef F_CPU
 #define F_CPU 16000000UL
-
+#endif
 
 #define set_bit(reg,bit) (reg |= (1 << bit))
 #define clear_bit(reg,bit) (reg &= ~(1 << bit))
 #define test_bit( reg, bit ) (reg & (1 << bit))
 
-#endif
+#define write_bit(val, reg, bit) (val ? set_bit(reg, bit) : clear_bit(reg, bit))
+
