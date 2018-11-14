22e7abd545fbe2059dfccf1d4eb48552af15eb44
Magnus Danielsen <magnudan@stud.ntnu.no>
2018-11-14 20:09:34 +0100

    Motor cleanup

--- node_2/motor_driver.h
+++ node_2/motor_driver.h
@@ -3,18 +3,21 @@
 
 #include <avr/io.h>
 
-
 #endif
 
 void motor_init();
 void motor_set_direction_left();
 void motor_set_direction_right();
 void motor_set_speed(uint8_t speed);
+
+//To be used with the joystick from USB mulitfunction card.
+//Slight tuning of the joystick threshold may be needed.
 void motor_set_speed_from_joystick(uint8_t joystick_output);
 
 
 void solenoid_init();
 void solenoid_punch(int signal);
 
+//Discrete implementations of PID-regulators
 void position_regulator(uint8_t ref_position);
 void speed_regulator(int8_t ref_speed);
