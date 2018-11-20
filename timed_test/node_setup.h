bb10bb62e5f81b7b9aa57c3a042497959d1bde16
Magnus Danielsen <magnudan@stud.ntnu.no>
2018-11-20 23:14:22 +0100

    Delete node_setup.h

--- timed_test/node_setup.h
+++
@@ -1,9 +0,0 @@
-#ifndef F_CPU
-#define F_CPU 16000000UL
-
-#endif
-
-#define set_bit(reg,bit) (reg |= (1 << bit))
-#define clear_bit(reg,bit) (reg &= ~(1 << bit))
-#define test_bit(reg, bit) (reg & (1 << bit))
-#define bit_write(val, reg, bit) (val ? set_bit(reg,bit) : clear_bit(reg,bit))
